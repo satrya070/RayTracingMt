2d51ebbcf06960ef2b518d09bbf14faa626d7bad
satrya070 <satrya@spotr.ai>
2025-01-05 22:50:00 +0100

    added vec3 class

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@ int main()
 	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
 	for (int j = 0; j < image_height; j++) {
+		std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
 		for (int i = 0; i < image_width; i++) {
 			auto r = double(i) / (image_width - 1);
 			auto g = double(j) / (image_height - 1);
@@ -21,4 +22,6 @@ int main()
 			std::cout << ir << ' ' << ig << ' ' << ib << '\n';
 		}
 	}
+
+	std::clog << "\r Done. \n";
 }
