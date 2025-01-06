e4d2d0fbfb8a7a060edeeb03659f75c7a01fe61d
satrya070 <satrya@spotr.ai>
2025-01-06 20:26:25 +0100

    added a... sphere

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,19 @@
 
 #include <iostream>
 
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+	vec3 oc = center - r.origin();
+	auto a = dot(r.direction(), r.direction());
+	auto b = -2.0 * dot(r.direction(), oc);
+	auto c = dot(oc, oc) - radius * radius;
+	auto discriminant = b * b - 4 * a * c;
+	return (discriminant >= 0);
+}
+
 color ray_color(const ray& r) {
+	if(hit_sphere(point3(0, 0, -1), 0.5, r))
+		return color(1, 0, 0);
+
 	vec3 unit_direction = unit_vector(r.direction());
 	auto a = 0.5 * (unit_direction.y() + 1.0);
 	return (1.0 - a) * color(1.0, 1.0, 1.0) * a * color(0.5, 0.7, 1.0);
@@ -23,7 +35,7 @@ int main()
 	// camera
 	auto focal_length = 1.0;
 	auto viewport_height = 2.0;
-	auto viewport_width = viewport_height * (double(image_width) / image_height);
+	auto viewport_width = viewport_height * (double(image_width) / image_height); // image dims as ratio could be slightly off due to rounding
 	auto camera_center = point3(0, 0, 0);
 
 	// calculate vector accross the horizontal and vertical edges
@@ -45,7 +57,7 @@ int main()
 		std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
 		for (int i = 0; i < image_width; i++) {
 
-			auto pixel_center = pixel00_loc + (i * pixel_delta_v) + (j * pixel_delta_v);
+			auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
 			auto ray_direction = pixel_center - camera_center;
 			ray r(camera_center, ray_direction);
 			
