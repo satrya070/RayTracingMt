816af40bed84afe82b61e8e0cdbcfe5f8b285a32
satrya070 <satrya@spotr.ai>
2025-01-07 13:30:06 +0100

    simplified hit spehere equation

--- src/main.cpp
+++ src/main.cpp
@@ -6,17 +6,20 @@
 
 double hit_sphere(const point3& center, double radius, const ray& r) {
 	vec3 oc = center - r.origin(); // vector from ray start to center
-	auto a = dot(r.direction(), r.direction());  // length of ray squared
-	auto b = -2.0 * dot(r.direction(), oc); // 
-	auto c = dot(oc, oc) - radius * radius;
-	auto discriminant = b * b - 4 * a * c;
+	auto a = r.direction().length_squared();
+	auto h = dot(r.direction(), oc);
+	auto c = oc.length_squared() - radius * radius;
+	//auto b = -2.0 * dot(r.direction(), oc); // 
+	//auto c = dot(oc, oc) - radius * radius;
+	//auto discriminant = b * b - 4 * a * c;
+	auto discriminant = h * h - a * c;
 	
 	// if 0 the function has 1 root, if >0 it has 2 distinct roots
 	if (discriminant < 0) {
 		return -1.0;
 	}
 	else {
-		return (-b - std::sqrt(discriminant)) / (2.0 * a);
+		return (h - std::sqrt(discriminant)) / a;
 	}
 }
 
