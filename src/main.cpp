31ed8b11c88b59542ade4f8101b76e16bd5ddc41
satrya070 <satrya@spotr.ai>
2025-01-07 13:14:08 +0100

    fix color setting

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,13 @@
 #include <iostream>
 
 double hit_sphere(const point3& center, double radius, const ray& r) {
-	vec3 oc = center - r.origin();
-	auto a = dot(r.direction(), r.direction());
-	auto b = -2.0 * dot(r.direction(), oc);
+	vec3 oc = center - r.origin(); // vector from ray start to center
+	auto a = dot(r.direction(), r.direction());  // length of ray squared
+	auto b = -2.0 * dot(r.direction(), oc); // 
 	auto c = dot(oc, oc) - radius * radius;
 	auto discriminant = b * b - 4 * a * c;
 	
+	// if 0 the function has 1 root, if >0 it has 2 distinct roots
 	if (discriminant < 0) {
 		return -1.0;
 	}
@@ -29,7 +30,7 @@ color ray_color(const ray& r) {
 
 	vec3 unit_direction = unit_vector(r.direction());
 	auto a = 0.5 * (unit_direction.y() + 1.0);
-	return (1.0 - a) * color(1.0, 1.0, 1.0) * a * color(0.5, 0.7, 1.0);
+	return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
 }
 
 int main()
