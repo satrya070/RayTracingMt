647cdbf9288608db6cef34a902f23298e638ca0f
satrya070 <satrya@spotr.ai>
2025-01-14 20:44:01 +0100

    gamma correction and lambertian

--- headers/camera.h
+++ headers/camera.h
@@ -8,6 +8,7 @@ class camera {
 		double aspect_ratio = 1.0; // ratio width over height
 		int image_width = 100; // rendered image width in pixels
 		int samples_per_pixel = 10; // count of random samples for each pixel
+		int max_depth = 10; // max limit of ray bounces
 
 		// public camera params
 		void render(const hittable& world) {
@@ -21,7 +22,7 @@ class camera {
 					color pixel_color(0, 0, 0);
 					for (int sample = 0; sample < samples_per_pixel; sample++) {
 						ray r = get_ray(i, j);
-						pixel_color += ray_color(r, world);
+						pixel_color += ray_color(r, max_depth, world);
 					}
 					write_color(std::cout, pixel_samples_scale * pixel_color);
 				}
@@ -65,12 +66,16 @@ class camera {
 			pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v); // places in 'the middle' of the pixel
 		}
 
-		color ray_color(const ray& r, const hittable& world) const {
+		color ray_color(const ray& r, int depth, const hittable& world) const {
+			// max limit bounces reached
+			if (depth <= 0)
+				return color(0, 0, 0);
+
 			hit_record rec;
-			if (world.hit(r, interval(0, infinity), rec)) {
+			if (world.hit(r, interval(0.001, infinity), rec)) {
 				// from given ray hitpoint p, shoot another ray from in random direction from p. until no more hits
-				vec3 direction = random_on_hemisphere(rec.normal);
-				return 0.5 * ray_color(ray(rec.p, direction), world);
+				vec3 direction = rec.normal + random_unit_vector(); //random_on_hemisphere(rec.normal);
+				return 0.5 * ray_color(ray(rec.p, direction), depth-1, world);
 			}
 
 			vec3 unit_direction = unit_vector(r.direction());
