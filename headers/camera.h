8cc96eaa856a2b785a9c2b3fb6891678779d3cb5
satrya070 <satrya@spotr.ai>
2025-01-14 19:59:44 +0100

    first render of diffuse spehre

--- headers/camera.h
+++ headers/camera.h
@@ -68,7 +68,9 @@ class camera {
 		color ray_color(const ray& r, const hittable& world) const {
 			hit_record rec;
 			if (world.hit(r, interval(0, infinity), rec)) {
-				return 0.5 * (rec.normal + color(1, 1, 1));
+				// from given ray hitpoint p, shoot another ray from in random direction from p. until no more hits
+				vec3 direction = random_on_hemisphere(rec.normal);
+				return 0.5 * ray_color(ray(rec.p, direction), world);
 			}
 
 			vec3 unit_direction = unit_vector(r.direction());
