daf40f1b6bd83087c24328757f2e6b3c40ebb3ea
satrya070 <satrya@spotr.ai>
2025-01-15 22:40:02 +0100

    add material, metaland add more spheres

--- headers/camera.h
+++ headers/camera.h
@@ -2,6 +2,7 @@
 #define CAMERA_H
 
 #include "hittable.h"
+#include "material.h"
 
 class camera {
 	public:
@@ -74,8 +75,11 @@ class camera {
 			hit_record rec;
 			if (world.hit(r, interval(0.001, infinity), rec)) {
 				// from given ray hitpoint p, shoot another ray from in random direction from p. until no more hits
-				vec3 direction = rec.normal + random_unit_vector(); //random_on_hemisphere(rec.normal);
-				return 0.5 * ray_color(ray(rec.p, direction), depth-1, world);
+				ray scattered;
+				color attenuation;
+				if (rec.mat->scatter(r, rec, attenuation, scattered))
+					return attenuation * ray_color(scattered, depth - 1, world);
+				return color(0, 0, 0);
 			}
 
 			vec3 unit_direction = unit_vector(r.direction());
