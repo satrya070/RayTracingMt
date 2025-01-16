88a533d658289d053d975388a0502d9445aa4160
satrya070 <satrya@spotr.ai>
2025-01-16 18:48:54 +0100

    add glass materials

--- headers/material.h
+++ headers/material.h
@@ -59,8 +59,19 @@ class dielectric : public material {
 		bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const override {
 			attenuation = color(1.0, 1.0, 1.0);
 			double ri = rec.front_face ? (1.0 / refraction_index) : refraction_index;
-
+			
 			vec3 unit_direction = unit_vector(r_in.direction());
+			double cos_theta = std::fmin(dot(-unit_direction, rec.normal), 1.0);
+			double sin_theta = std::sqrt(1.0 - cos_theta * cos_theta);
+
+			bool cannot_refract = ri * sin_theta > 1.0;
+			vec3 direction;
+
+			if (cannot_refract || reflectance(cos_theta, ri) > random_double())
+				direction = reflect(unit_direction, rec.normal);
+			else
+				direction = refract(unit_direction, rec.normal, ri);
+
 			vec3 refracted = refract(unit_direction, rec.normal, ri);
 
 			scattered = ray(rec.p, refracted);
@@ -71,6 +82,13 @@ class dielectric : public material {
 		// refractive index in vaccuum or air, or ratio of the material's refractive index over
 		// the refractive index of the enclosing media << what ever that means.
 		double refraction_index;
+
+		static double reflectance(double cosine, double refraction_index) {
+			// use schlicks approximation for reflectance
+			auto r0 = (1 - refraction_index) / (1 + refraction_index);
+			r0 = r0 * r0;
+			return r0 + (1 - r0) * std::pow((1 - cosine), 5);
+		}
 };
 
 #endif
